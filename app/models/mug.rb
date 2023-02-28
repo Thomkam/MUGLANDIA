2f7514b0cb18463d107212bc756a9b5a5c3a8c44
benoit lebon <ben142007@live.fr>
2023-02-28 10:52:14 +0100

    mug controller

--- app/models/mug.rb
+++ app/models/mug.rb
@@ -1,3 +1,6 @@
 class Mug < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true
+  validates :localisation, presence: true
+  validates :localisation, length: { minimum: 2 }
 end
